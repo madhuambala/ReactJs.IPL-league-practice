de2d67e1ba21536d6d93879feecca1af0322a915
madhuambala <amadhusudhan369@gmail.com>
2023-04-19 13:20:05 +0000

    reactJs leaguercb and csk

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <div className="container">
+      <h1 className="heading">Super Over League</h1>
+      <div>
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
